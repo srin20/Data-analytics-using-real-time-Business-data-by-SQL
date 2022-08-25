80f33eb8622c3ed88a6329917eed6e54751ac0cd
srin20 <111554079+srin20@users.noreply.github.com>
2022-08-25 17:22:10 +0530

    Update employee_sql_query.sql

--- employee_sql_query.sql
+++ employee_sql_query.sql
@@ -29,3 +29,28 @@ WHERE
 select * from employees where first_name in ('John', 'Arjun');
 
 select * from employees where first_name not in('John', 'Mark', 'Arjun');
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN 66000 AND 70000;
+
+   
+
+SELECT 
+    *
+FROM
+    employees_upload
+WHERE
+    emp_no NOT BETWEEN '10003' AND '10009';
+
+   
+
+SELECT 
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no BETWEEN 'd003' AND 'd006';
